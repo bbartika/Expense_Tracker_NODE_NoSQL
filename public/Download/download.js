ac1eda1dc82964cb51ff3618f1c4b73db6afcb88
bbartika <137605070+bbartika@users.noreply.github.com>
2024-07-08 10:53:32 +0530

    Add files via upload

---
+++ public/Download/download.js
@@ -0,0 +1,36 @@
+
+const token=localStorage.getItem('token')
+let sno=1
+axios.get('http://localhost:3000/expense/getdownloadedURLS',{headers:{"Authorization":token}}).then((res)=>{
+ const downloadedFiles=res.data.downloadedFiles
+ for(let file of downloadedFiles){
+     const tbody=document.querySelector('#tbody')
+
+     const date = new Date(file.date);
+     const formattedDate = date.toISOString().split("T")[0]
+
+     const a=document.createElement('a')
+     a.href=file.URL
+     a.innerHTML = "Click to Download Again";
+
+     
+     const tr=document.createElement('tr')
+     tr.innerHTML=`
+     <td>${sno++}</td>
+     <td>${formattedDate}</td>
+      <td>${a.outerHTML}</td>
+     `
+     tbody.appendChild(tr)
+          
+ }
+
+}).catch((error) => {
+ if ( error.response.data.error) {
+     
+     alert(error.response.data.error);
+ } else {
+
+     console.error(error);
+ }
+});
+
