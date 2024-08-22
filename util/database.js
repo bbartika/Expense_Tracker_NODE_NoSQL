4bb9f7a67853f29b852822a195f27750395371e5
bbartika <137605070+bbartika@users.noreply.github.com>
2024-08-22 12:55:46 +0530

    Using Mongodb

--- util/database.js
+++ util/database.js
@@ -1,7 +1,6 @@
 const Sequelize=require('sequelize').Sequelize
 require('dotenv').config();
 
-
 const sequelize=new Sequelize('expense-tracker','root','uitIT$1822',{
     dialect:'mysql',
     host:'localhost',
